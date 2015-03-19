8e981797300acf1414c7769428d38fc0a63b2b8a
Anil Madhavapeddy <anil@recoil.org>
2015-03-19 14:38:45 +0000

    sync latest upstream changes

--- gen-submodules.sh
+++ gen-submodules.sh
@@ -3,5 +3,5 @@
 for i in `cat TROVE`; do
   user=`echo $i | awk -F'/' '{print $1}'`
   repo=`echo $i | awk -F'/' '{print $2}'`
-  echo git submodule add https://github.com/$user/$repo
+  git submodule add https://github.com/$user/$repo || true
 done
