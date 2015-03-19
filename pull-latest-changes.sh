7e49cad2ba73601127e3b33a4df5a22bc252bb34
Anil Madhavapeddy <anil@recoil.org>
2015-03-19 14:41:42 +0000

    source the ssh-agent early

--- pull-latest-changes.sh
+++ pull-latest-changes.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
 ./gen-submodules.sh
+. $HOME/.keychain/bactrian-sh
 git pull -q
 git submodule init -q
 git submodule update -q
 git submodule foreach git pull origin master -q
 git commit -m 'sync latest upstream changes' -a -q || true
-. $HOME/.keychain/bactrian-sh
 git push -q || true
